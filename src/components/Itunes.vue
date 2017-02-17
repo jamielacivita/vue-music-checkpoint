d28b19c43cad219e15e164635418a495817e24bf
brawlingNavada <JCLacivita@gmail.com>
2017-02-17 11:01:52 -0700

    able to identify index of selected song

--- src/components/Itunes.vue
+++ src/components/Itunes.vue
@@ -5,11 +5,16 @@
 <h3>Enter Artist: <input type="text" v-model="artist"></h3>
 <button type="submit">Submit</button>    
 </form>
+<!--<h4>{{requestResponse}}</h4>-->
+<h4 v-for="(song, index) in requestResponse">
+    <button @click="testMethod(index)">To My List</button>
+    {{song.trackName}}</h4>
 </div>
 </template>
 
 <script>    
 import ItunesService from '../services/itunes-service.js'
+import MyTunesService from '../services/mytunes-service.js'
 export default {
   name: 'home',
   data () {
@@ -21,9 +26,22 @@ export default {
   },
   methods: {
 requestMusic(){
-    ItunesService.getMusicByArtist(this.artist)
+    ItunesService.getMusicByArtist(this.artist).then(response => {
+  // Convert to JSON
+	return response.json();
+}).then(j => {
+	// Yay, `j` is a JavaScript object
+	//console.log(j);
+  //return j
+  this.requestResponse = j['results']
+});},
 
+testMethod(index)
+{
+    console.log("In test Method with", index)
 }
+
+
   }
 }
 </script>
@@ -31,3 +49,13 @@ requestMusic(){
 <style> 
 
 </style>
+
+ <!--ItunesService.getMusicByArtist(this.artist).then(function(response) {
+  // Convert to JSON
+	return response.json();
+}).then(function(j) {
+	// Yay, `j` is a JavaScript object
+	console.log(j);
+  //return j
+  this.requestResponse = j
+});-->
--- src/services/itunes-service.js
+++ src/services/itunes-service.js
@@ -4,14 +4,7 @@ export default {
     var url2 = 'https://itunes.apple.com/search?term=' + artist;
     var apiUrl = url + encodeURIComponent(url2);
     return fetch(apiUrl)
-    .then(function(response) {
-  // Convert to JSON
-	return response.json();
-}).then(function(j) {
-	// Yay, `j` is a JavaScript object
-	console.log(j);
-  return j
-});
+    //goes here
   
   }
 }
